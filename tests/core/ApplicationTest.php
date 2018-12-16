7c3c51f18126fdf7f41ee3ab61e895621c0fc07c
poluxGit <poluxmsn@free.Fr>
2018-12-16 14:35:12 +0100

    CI Setup - Déplacement de l'arborescence des tests PHP. - Initialisation du test de la classe Application

---
+++ tests/core/ApplicationTest.php
@@ -0,0 +1,22 @@
+<?php
+use PHPUnit\Framework\TestCase;
+use GOM\Core\Application;
+
+/**
+ * ApplicationTest
+ *
+ * Tests de la classe GOM\Core\Application
+ */
+final class ApplicationTest extends TestCase
+{
+		/**
+		 * testApplicationLoadInvalidSettingsFile
+		 *
+		 * Chargement des paramètres depuis un fichier invalide.
+		 */
+  	public function testApplicationLoadInvalidSettingsFile(): void
+  	{
+			$this->expectException(Exception::class);
+			Application::loadDBSettings('./toto');
+	  }//end testApplicationLoadInvalidSettingsFile()
+}//end class
