812633e70891a0a75ddfb72e0e9897618c52549f
poluxGit <poluxmsn@free.Fr>
2018-12-22 19:56:35 +0100

    t

--- src/core/data/Model.php
+++ src/core/data/Model.php
@@ -11,7 +11,8 @@ class Model extends Internal\GOMObject
   /**
    * Constructeur par défaut
    */
-  public function __construct(string $psTID){
+  public function __construct(string $psTID)
+  {
     parent::__construct($psTID, 'A000_MDL');
     $this->initFieldDefinition();
   }//end __construct()
@@ -24,17 +25,67 @@ class Model extends Internal\GOMObject
   {
     $this->addFieldDefinition('ID', 'BID', 'string', 'Id. (BID)');
     $this->addFieldDefinition('CODE', 'CODE', 'string', 'Code du model');
-    $this->addFieldDefinition('CodeBID', 'BIDCODE', 'string', 'Code appliqué au BID');
-    $this->addFieldDefinition('Version', 'VERSION', 'string', 'Version du model');
-    $this->addFieldDefinition('TitreCourt', 'STITLE', 'string', 'Titre Court');
+    $this->addFieldDefinition(
+        'CodeBID',
+        'BIDCODE',
+        'string',
+        'Code appliqué au BID'
+    );
+    $this->addFieldDefinition(
+        'Version',
+        'VERSION',
+        'string',
+        'Version du model'
+    );
+    $this->addFieldDefinition(
+        'TitreCourt',
+        'STITLE',
+        'string',
+        'Titre Court'
+    );
     $this->addFieldDefinition('TitreLong', 'LTITLE', 'string', 'Titre Long');
-    $this->addFieldDefinition('Commentaire', 'COMMENT', 'string', 'Commentaire divers');
-    $this->addFieldDefinition('JSONData', 'JSON_DATA', 'string', 'Données complémentaires');
-    $this->addFieldDefinition('DateCreation', 'CDATE', 'date', 'Date de création');
-    $this->addFieldDefinition('DateMaj', 'UDATE', 'date', 'Date de dernière maj');
-    $this->addFieldDefinition('UserCreation', 'CUSER', 'string', 'Compte Utilisateur du créateur');
-    $this->addFieldDefinition('UserMaj', 'UUSER', 'string', 'Compte Utilisateur de l\'updateur');
-    $this->addFieldDefinition('EstSupprime', 'IS_DELETED', 'INT', 'Flag de suppression');
+    $this->addFieldDefinition(
+        'Commentaire',
+        'COMMENT',
+        'string',
+        'Commentaire divers'
+    );
+    $this->addFieldDefinition(
+        'JSONData',
+        'JSON_DATA',
+        'string',
+        'Données complémentaires'
+    );
+    $this->addFieldDefinition(
+        'DateCreation',
+        'CDATE',
+        'date',
+        'Date de création'
+    );
+    $this->addFieldDefinition(
+        'DateMaj',
+        'UDATE',
+        'date',
+        'Date de dernière maj'
+    );
+    $this->addFieldDefinition(
+        'UserCreation',
+        'CUSER',
+        'string',
+        'Compte Utilisateur du créateur'
+    );
+    $this->addFieldDefinition(
+        'UserMaj',
+        'UUSER',
+        'string',
+        'Compte Utilisateur de l\'updateur'
+    );
+    $this->addFieldDefinition(
+        'EstSupprime',
+        'IS_DELETED',
+        'INT',
+        'Flag de suppression'
+    );
   }//end initFieldDefinition()
 
   // ************************************************************************ //
