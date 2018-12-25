bcc176d4ac48f134289dbc8c48f18006c1cfb982
poluxGit <poluxmsn@free.Fr>
2018-12-25 23:44:22 +0100

    Maj

--- tests/core/DataFactoryTest.php
+++ tests/core/DataFactoryTest.php
@@ -1,7 +1,8 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
-use GOM\Data\Internal;
+use GOM\Core\DataFactory;
+use GOM\Core\Application;
 
 /**
  * DataFactory - Classe statique de gestion des données
@@ -10,16 +11,26 @@ use GOM\Data\Internal;
 class DataFactoryTest extends TestCase
 {
   /**
-   * testApplicationLoadInvalidSettingsFile
+   * testDeployApplicationCoreDatabaseStructure
    *
    * Chargement des paramètres depuis un fichier invalide.
    * @expectedException GOM\Core\Internal\Exception\ApplicationSettingsFileNotFoundException
    */
-  public function testApplicationLoadSettingsFromNonExistingFile(): void
+  public function testDeployApplicationCoreDatabaseStructure(): void
   {
-    $this->markTestIncomplete(
-          'This test has not been implemented yet.'
-        );
-  }//end testApplicationLoadSettingsFromNonExistingFile()
+    Application::loadDBSettings('./../tests/datasets/AppSettingsFile_02-valid.json');
+    Application::deploySchemaToDefaultAppliDB(
+      'root',
+      'dev'
+    );
+    $this->assertTrue(true);
+  }//end testDeployApplicationCoreDatabaseStructure()
+
+  public function testGettingModelFromDatabaseByTid()
+  {
+    $lObj = DataFactory::getModel('MDL-SI-0001');
+    $this->assertNotNull($lObj);
+    $this->assertInstanceOf(GOM\Core\Data\Model::class,$lObj);
+  }
 }//end class
  ?>
